d9a4c0eb84e3eadc62279d0b7c45b47c851b7931
Saori Shigehisa <kumakichi3173@gmail.com>
2021-05-06 13:56:17 -0400

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -50,8 +50,7 @@ def read_temp():
         temp_string = temp[pos+2:]
         temp_c = float(temp_string)/1000.0 
         temp_f = temp_c * (9.0 / 5.0) + 32.0
-        return temp_c, temp_f
-            
+        return temp_c, temp_f            
  
 print(' ROM: '+ rom)
 
